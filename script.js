5ebd9c9d61d32c5deac7c1dae138a7ea38e33fba
sanjarabidjanov <sanjarabidjanov240@gmail.com>
2025-06-26 15:38:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let random = (Math.random() * 100)
+let son = random * 2;
+let son2 = son / 2;
+let son3 = Math.round(son2)
+
+console.log(random);
+console.log(son);
+console.log(son2);
+console.log(son3);
+alert(son3)
